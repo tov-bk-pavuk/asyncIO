c6b663a70ad85306321ce50ccfd9a4d2f8e920ce
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-11-16 15:45:10 +0200

    использовал gather

--- main.py
+++ main.py
@@ -16,17 +16,34 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, urls[0])
-        avg_mono = (html[0]['rateBuy'] + html[0]['rateSell']) / 2
+        # html = await fetch(session, urls[0])
+        # avg_mono = (html[0]['rateBuy'] + html[0]['rateSell']) / 2
+        #
+        # html = await fetch(session, urls[1])
+        # nac_bank = html[26]['rate']
+        #
+        # html = await fetch(session, urls[2])
+        # privat_bank = (float(html[0]['buy']) + float(html[0]['sale']))/2
 
-        html = await fetch(session, urls[1])
-        nac_bank = html[26]['rate']
+        result = await asyncio.gather(
+            fetch(session, urls[0]),
+            fetch(session, urls[1]),
+            fetch(session, urls[2]),
+        )
+        print('Пришли результаты корутинов:')
 
-        html = await fetch(session, urls[2])
-        privat_bank = (float(html[0]['buy']) + float(html[0]['sale']))/2
+        avg_mono = (result[0][0]['rateBuy'] + result[0][0]['rateSell']) / 2
+        nac_bank = result[1][26]['rate']
+        privat_bank = (float(result[2][0]['buy']) + float(result[2][0]['sale'])) / 2
 
-        print(f' средний курс USD/ГРН за 1 USD = {round((avg_mono + nac_bank + privat_bank)/3, 2)} грн')
+        print(f'\nМоно: {avg_mono}, грн')
+        print(f'Нац банк: {nac_bank}, грн')
+        print(f'ПриватБанк: {privat_bank}, грн')
+
+        print(f'Средний курс USD/ГРН за 1 USD = {round((avg_mono + nac_bank + privat_bank)/3, 2)} грн')
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+asyncio.run(main())
+
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(main())
